aa37feeff4acf61f7331634e4956df6bc7a8e489
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-08-09 22:40:04 -0300

    feat: add initial implementation of the Trie

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
-pub fn hello_world() {
-    println!("Hello world!");
+use std::collections::HashMap;
+
+struct Node {
+    next: HashMap<char, u64>,
+    leaf: bool,
+}
+
+struct Trie {
+    trie: Vec<Node>,
+}
+
+impl Node {
+    pub fn new() -> Node {
+        Node { next: HashMap::new(), leaf: false }
+    }
+}
+
+impl Trie {
+    pub fn new() -> Trie {
+        Trie {
+            trie: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, word: &str) {
+        todo!()
+    }
+
+    pub fn exists(&self, word: &str) -> bool {
+        false
+    }
 }
 
 #[cfg(test)]
@@ -7,7 +36,11 @@ mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        hello_world();
+    pub fn check_exists() {
+        let mut trie = Trie::new();
+
+        trie.add("foo");
+        
+        assert!(trie.exists("foo"));
     }
 }
