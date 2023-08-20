f4532428ea1342cec38e6e94645fdb30893c6715
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-08-20 00:32:00 -0300

    feat: update App component with router configuration

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,15 @@
+use crate::views::{about::About, home::Home, not_found::NotFound};
 use leptos::*;
 use leptos_router::*;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     view! { cx,
-        <Router>
-            <main>
-                <p>"Hello, World!"</p>
-            </main>
+        <Router fallback=|cx| view! { cx, <NotFound /> }>
+            <Routes>
+                <Route path="/" view=Home />
+                <Route path="/about" view=About />
+            </Routes>
         </Router>
     }
 }
